9c74188220191ea5dafae238e674cd20deb9cfc8
AClon314 <30747832+AClon314@users.noreply.github.com>
2024-07-20 17:04:15 +0800

    add prev and tool.html

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-# phy_js
+# phy_js Usage使用方法
 使用`matter-js`，教育小孩用的js物理demo合集。
+打开`tool.html`即可。`prev.html`是仅预览。
 
 ## Add添加
 1. 添加：在/examples下按basic.js添加一个js
--- src/demo.js
+++ src/demo.js
@@ -1,45 +1,54 @@
 var sourceLinkRoot = 'https://github.com/liabru/matter-tools/blob/master';
 
-MatterTools.Demo.create({
-  fullPage: true,
-  preventZoom: true,
-  startExample: true,
-  appendTo: document.body,
-
-  toolbar: {
-    title: 'matter-tools',
-    url: 'https://github.com/liabru/matter-tools',
-    reset: true,
-    source: true,
-    inspector: true,
-    tools: true,
-    fullscreen: true,
-    exampleSelect: true
-  },
-
-  tools: {
-    inspector: true,
-    gui: true
-  },
-  
-  examples: [
-    {
-      name: 'balance pan',
-      id: 'pan',
-      init: Example.pan,
-      sourceLink: './docs/examples/pan.js'
-    },
-    {
-      name: 'Basic',
-      id: 'basic',
-      init: Example.basic,
-      sourceLink: sourceLinkRoot + '/docs/examples/basic.js'
-    },
-    {
-      name: 'Basic 2',
-      id: 'basic-2',
-      init: Example.basic,
-      sourceLink: sourceLinkRoot + '/docs/examples/basic.js'
+// if MatterTools is not defined
+if (typeof MatterTools === 'undefined' && Example) {
+  for (var key in Example) {
+    if (typeof Example[key] === 'function') {
+      Example[key]();
     }
-  ]
-});
+  }
+} else {
+  MatterTools.Demo.create({
+    fullPage: true,
+    preventZoom: true,
+    startExample: true,
+    appendTo: document.body,
+
+    toolbar: {
+      title: 'matter-tools',
+      url: 'https://github.com/liabru/matter-tools',
+      reset: true,
+      source: true,
+      inspector: true,
+      tools: true,
+      fullscreen: true,
+      exampleSelect: true
+    },
+
+    tools: {
+      inspector: true,
+      gui: true
+    },
+
+    examples: [
+      {
+        name: 'balance pan',
+        id: 'pan',
+        init: Example.pan,
+        sourceLink: './docs/examples/pan.js'
+      },
+      {
+        name: 'Basic',
+        id: 'basic',
+        init: Example.basic,
+        sourceLink: sourceLinkRoot + '/docs/examples/basic.js'
+      },
+      {
+        name: 'Basic 2',
+        id: 'basic-2',
+        init: Example.basic,
+        sourceLink: sourceLinkRoot + '/docs/examples/basic.js'
+      }
+    ]
+  });
+}
